c6ece66f186c535c6dd1e20ebc1091628d91b8c8
katepepper <60437068+katepepper@users.noreply.github.com>
2020-02-07 09:08:22 -0600

    update name_cases.py

--- name_cases.py
+++ name_cases.py
@@ -1,9 +1,9 @@
-# Course:
-# Period:
-# Date created:
-# Date last modified:
-# Name:
-# Desrcription:
+# Course: CS 30
+# Period: 1
+# Date created: 2020/02/05
+# Date last modified: 2020/02/07
+# Name: Kate Pepper
+# Desrcription: Prints ot a question asking if I am ready to start
 
 name = "kate"
 message = f"Hello, {name.title()} are you ready to start?"
